93dd99004ccd689216a0772b7f5d02788db1116f
nehoray <nehoray99@gmail.com>
2020-08-16 22:41:13 +0300

    datepicker add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,11 @@ import { Container, Typography } from "@material-ui/core";
 import React, { Component } from "react";
 import "./App.css";
 import CollapsibleTablele from "./components/Table";
-
 class App extends Component {
   render() {
     return (
       <Container maxWidth="sm">
-        <Typography variant="h3" gutterBottom>
+        <Typography variant="h3" gutterBottom align="center">
           Dininig Room
         </Typography>
         <CollapsibleTablele />;
